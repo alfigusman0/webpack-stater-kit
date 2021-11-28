277b4a4feb0cd34ae51b05732f4d5d253ce5c0c4
Alfi Gusman <alfigusman0@gmail.com>
2021-11-29 00:12:08 +0700

    fix source code and add convert datetime

--- application/libraries/Utilities.php
+++ application/libraries/Utilities.php
@@ -26,7 +26,7 @@ class Utilities
     }
 
     /* Send Email */
-    public function sendEmail($username, $password, $from = "contact@uinsgd.ac.id", $cc = "admin.pmb@uinsgd.ac.id", $title = "No-Reply - UIN Sunan Gunung Djati", $subject = "UIN Sunan Gunung Djati Bandung", $email, $content)
+    public function sendEmail($username, $password, $from = "alfi.gusman.9f@gmail.com", $cc = "alfi.gusman.9f@gmail.com", $title = "No-Reply - Web Apps", $subject = "Web Apps", $email, $content)
     {
         $config = array(
             'protocol' => 'smtp',
@@ -108,5 +108,16 @@ class Utilities
         curl_close($ch);
         $respond = json_decode($respond);
         return $respond;
+    }
+
+    public function convertDateTime($date, $format = 'Y-m-d H:i:s')
+    {
+        $tz1 = 'UTC';
+        $tz2 = 'Asia/Jakarta'; // UTC +7
+
+        $d = new DateTime($date, new DateTimeZone($tz1));
+        $d->setTimeZone(new DateTimeZone($tz2));
+
+        return $d->format($format);
     }
 }
